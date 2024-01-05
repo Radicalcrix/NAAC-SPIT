c936becf1b6ebbb3d67b372cd2a1df7881c96d77
Radicalcrix <ompatil254@gmail.com>
2024-01-05 14:51:42 +0530

    Footer

--- Naac-spit/src/App.jsx
+++ Naac-spit/src/App.jsx
@@ -1,15 +1,17 @@
 import Buttons from "./components/Buttons.jsx";
 import Header from "./components/Header.jsx";
 import CriteriaPage from "./pages/CriteriaPage";
+import Footer from "./components/footer.jsx";
 
 function App() {
   return (
     <>
-    <Header/>
-    <Buttons/>
-    <CriteriaPage/>
+      <Header />
+      <Buttons />
+      <CriteriaPage />
+      <Footer />
     </>
   );
-} 
+}
 
-export default App
+export default App;
